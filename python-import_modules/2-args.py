d7608c5e8e1a6edd65d6f75681346b2c95371fee
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-04 15:38:12 +0200

    Refactor argument printing logic to be encapsulated within the __main__ block

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 1:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print("1 argument:")
-    print(f"1: {sys.argv[1]}")
-else:
-    print("{} arguments: ".format(len(sys.argv) -1))
-    for i in range(1, len(sys.argv)):
-        print(f"{i}: ", sys.argv[i])
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print("{} arguments: ".format(len(sys.argv)-1))
+        for i in range(1, len(sys.argv)):
+            print(f"{i}: ", sys.argv[i])
