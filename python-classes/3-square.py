598fe3838bb681596b2e57b2c13c132b39f914bb
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-15 15:24:20 +0200

    Implement Square class with size validation and area method

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+class Square:
+    __size = None
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
