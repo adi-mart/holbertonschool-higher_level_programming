105ea4d00ff0883f2a98dc3cd539ae0576171502
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-15 15:40:45 +0200

    Implement Square class with size validation and area calculation methods

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+class Square:
+    """A class that defines a square by its size."""
+    __size = None
+
+    def __init__(self, size=0):
+        """Initialize the square with a given size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("value must be an integer")
+        if value < 0:
+            raise ValueError("value must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return (self.__size * self.__size)
