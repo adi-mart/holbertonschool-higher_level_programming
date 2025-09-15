dd3f80a999b8924ba7e8f62e24c9ea0ca379339f
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-15 11:32:46 +0200

    Add Square class implementations and main files for testing

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """A class that defines a square by its size."""
+    __size = None
+
+    def __init__(self, size):
+        """Initialize the square with a given size."""
+        self.__size = size
