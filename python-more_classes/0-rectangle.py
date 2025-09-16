c9b1112a5a3befe258d9c4905ed9008b142635b7
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-16 15:53:14 +0200

    Refactor Rectangle class docstring and remove unused __init__ method

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+"""Defines a rectangle."""
+
+
 class Rectangle:
-    """Defines a rectangle."""
-    def __init__(self, width=0, height=0):
-        """Initializes a Rectangle instance.
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        pass
+    """Represent a rectangle."""
+    pass
