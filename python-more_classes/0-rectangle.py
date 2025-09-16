f74394bee9434277e66fb8867f0e4125ec08d3c3
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-16 15:50:31 +0200

    Add initial implementation of Rectangle class and related test files

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Defines a rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initializes a Rectangle instance.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        pass
