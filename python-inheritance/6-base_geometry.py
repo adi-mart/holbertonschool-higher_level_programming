e5a5d3532ef7cd8c8dea84c1d780fd4be6756da6
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-22 14:43:18 +0200

    Add docstring to BaseGeometry class for clarity

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -3,7 +3,7 @@
 
 
 class BaseGeometry:
-
+    """An empty class."""
     def area(self):
         """Raises an Exception with the message 'area() is not implemented'."""
         raise Exception("area() is not implemented")
