84c6c27422fba25bef1b6d7a403e8f82ef60cb69
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-22 10:47:01 +0200

    Add docstring to lookup function and Add MyList class inheriting from list with print_sorted method

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+class MyList(list):
+    """A class that inherits from list."""
+    def print_sorted(self):
+        """Prints the list in sorted (ascending) order."""
+        print(self)
