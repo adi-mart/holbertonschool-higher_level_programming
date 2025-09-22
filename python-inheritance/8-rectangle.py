501b8b0297988597a1707623962300d2e46c8cfa
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-22 15:19:40 +0200

    Fix initialization order and enhance integer validation in Rectangle class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,14 +13,14 @@ class Rectangle(BaseGeometry):
     """A class that defines a rectangle."""
     def __init__(self, width, height):
         """Initializes the rectangle with width and height."""
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def integer_validator(self, name, value):
         """Validates that 'value' is a positive integer."""
-        if not isinstance(value, int):
+        if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
