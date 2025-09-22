1eb41b1dc3488ddb056ccd23e161a4c1b5b93a16
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-22 16:01:05 +0200

    Fix Square class initialization by moving size assignment after super() call

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -46,8 +46,8 @@ class Square(Rectangle):
     def __init__(self, size):
         """Initializes the square with size."""
         self.integer_validator("size", size)
-        self.__size = size
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         """Returns the area of the square."""
