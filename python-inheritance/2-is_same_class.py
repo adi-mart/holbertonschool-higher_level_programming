477c8360b153a30a8679aef9200bb65da84d80ca
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-22 14:21:27 +0200

    Add docstrings to lookup and is_same_class functions, implement print_sorted method in MyList class, and create tests for MyList functionality

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module that defines a function to check if an object is exactly
+an instance of a specified class."""
+
+
 def is_same_class(obj, a_class):
     """Returns True if the object is exactly an instance of
     the specified class; otherwise False.
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Module that defines a function to check if an object is an instance
+of a class that inherited from a specified class."""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False.
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return (True)
+    else:
+        return (False)
