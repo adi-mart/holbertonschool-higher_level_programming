239552c7183ad9f426f3e387f744c0732a50f63a
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-22 15:21:59 +0200

    Refactor integer_validator method to BaseGeometry class and remove duplicate implementation in Rectangle class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -8,6 +8,16 @@ class BaseGeometry:
         """Raises an Exception with the message 'area() is not implemented'."""
         raise Exception("area() is not implemented")
 
+    def integer_validator(self, name, value):
+        """Validates that 'value' is a positive integer."""
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""Module that defines a class Rectangle that inherits from BaseGeometry."""
+
 
 class Rectangle(BaseGeometry):
     """A class that defines a rectangle."""
@@ -17,10 +27,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def integer_validator(self, name, value):
-        """Validates that 'value' is a positive integer."""
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
