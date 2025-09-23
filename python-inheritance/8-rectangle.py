0de412c69e7df4b3954d5819d4f152c992f6414e
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-23 09:51:23 +0200

    Enhance integer_validator docstring in BaseGeometry class for clarity and detail

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -9,7 +9,15 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that 'value' is a positive integer."""
+        """Validates that 'value' is a positive integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If 'value' is not an integer.
+            ValueError: If 'value' is less than or equal to 0.
+        """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
