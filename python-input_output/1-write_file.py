7cb1462b039d28958a76302b04c9f54dd0c179b5
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-29 15:51:37 +0200

    Fix formatting in write_file docstring and update README file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,12 +6,13 @@ Module that contains a function to write to a text file
 
 def write_file(filename="", text=""):
     """
-    Write a string to a text file (UTF8) and return the number of characters written
-    
+    Write a string to a text file (UTF8) and return the number
+    of characters written
+
     Args:
         filename (str): name of the file to write to
         text (str): text to write to the file
- 
+
     Returns:
         int: number of characters written
     """
