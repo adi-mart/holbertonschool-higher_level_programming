43988890e4fe2de14e4c103730fa85589abb118b
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-29 15:42:23 +0200

    Implement append_write function to append text to a file and create initial text file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,13 +6,15 @@ Module that contains a function to append to a text file
 
 def append_write(filename="", text=""):
     """
-    Append a string at the end of a text file (UTF8) and return the number of characters added
-    
+    Append a string at the end of a text file (UTF8) and
+    return the number of characters added
+
     Args:
         filename (str): name of the file to append to
         text (str): text to append to the file
-        
+
     Returns:
         int: number of characters added
     """
-    pass
+    with open(filename, 'a', encoding='utf-8') as f:
+        return (f.write(text))
