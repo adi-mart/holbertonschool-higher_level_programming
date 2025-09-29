7cd97604e37cb7aca3c3f43292584c485313d005
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-29 15:49:33 +0200

    Implement from_json_string function to convert JSON string to Python object

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -2,16 +2,17 @@
 """
 Module that contains a function to convert JSON string to object
 """
+import json
 
 
 def from_json_string(my_str):
     """
     Return an object (Python data structure) represented by a JSON string
-    
+
     Args:
         my_str (str): JSON string to convert to object
-        
+
     Returns:
         object: Python data structure represented by JSON string
     """
-    pass
+    return (json.loads(my_str))
