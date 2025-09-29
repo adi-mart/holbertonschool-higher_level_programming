f7eea06ec2c51b4c3c641487391b889cee0f4718
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-29 15:47:09 +0200

    Fix JSON serialization function and update error handling in main script

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,16 +2,17 @@
 """
 Module that contains a function to convert object to JSON string
 """
+import json
 
 
 def to_json_string(my_obj):
     """
     Return the JSON representation of an object (string)
-    
+
     Args:
         my_obj: object to convert to JSON string
-        
+
     Returns:
         str: JSON representation of the object
     """
-    pass
+    return (json.dumps(my_obj))
