75d873d05200e7998d938e6b24fefa8ca1b298d1
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-30 09:16:29 +0200

    Implement Student class constructor and to_json method for instance representation

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -8,23 +8,25 @@ class Student:
     """
     Class that defines a student
     """
-    
+
     def __init__(self, first_name, last_name, age):
         """
         Initialize a Student instance
-        
+
         Args:
             first_name (str): first name of the student
             last_name (str): last name of the student
             age (int): age of the student
         """
-        pass
-    
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
     def to_json(self):
         """
         Retrieve a dictionary representation of a Student instance
-        
+
         Returns:
             dict: dictionary representation of the student
         """
-        pass
+        return (self.__dict__)
