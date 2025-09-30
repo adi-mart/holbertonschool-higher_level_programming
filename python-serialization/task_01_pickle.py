0fbff25e4de202846c59c0d7c29bed2f28093aae
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-09-30 14:33:51 +0200

    Implement CustomObject class with serialization and deserialization methods

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Custom object with pickle serialization capabilities."""
+import pickle
+
+
+class CustomObject:
+    """A custom class that can be serialized and deserialized using pickle."""
+
+    def __init__(self, name, age, is_student):
+        """Initialize a CustomObject instance.
+
+        Args:
+            name (str): The name of the person
+            age (int): The age of the person
+            is_student (bool): Whether the person is a student
+        """
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display the object's attributes in the specified format."""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize the current instance of the object to a file.
+
+        Args:
+            filename (str): The filename to save the serialized object to
+        """
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize an object from a file.
+
+        Args:
+            filename (str): The filename to load the serialized object from
+
+        Returns:
+            CustomObject or None: The deserialized object or None if error
+        """
+        with open(filename, 'rb') as f:
+            loaded_data = pickle.load(f)
+        return (loaded_data)
