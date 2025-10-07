4c7f3b9c5ef3c337822692932e11b4e0d155b901
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-07 15:23:42 +0200

    Fix status code print formatting in fetch_and_print_posts and fetch_and_save_posts functions

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -15,7 +15,7 @@ def fetch_and_print_posts():
     Also prints the status code of the response.
     """
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"(Status Code): {response.status_code}")
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         for i in posts:
@@ -28,7 +28,7 @@ def fetch_and_save_posts():
     Creates posts.csv with columns: id, title, body
     """
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"(Status Code): {response.status_code}")
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         with open('posts.csv', "w", newline="") as csvfile:
