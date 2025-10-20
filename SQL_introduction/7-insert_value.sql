11fd22b3c66fd8919435c6ac35b5525eb0368718
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-20 16:12:46 +0200

    Fix SQL syntax in 7-insert_value.sql for correct row insertion

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,7 +1,5 @@
 -- Description: Inserts a row into first_table.
 -- Usage: cat 7-insert_value.sql | mysql -hlocalhost -uroot -p [database_name]
 
-INSERT INTO first_table (
-    id 89,
-    name Best School
-);
+INSERT INTO first_table (id, name)
+VALUES (89, "Best School");
