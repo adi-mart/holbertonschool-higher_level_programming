1297c3b557b4f251c066aa4e31525bf2627d56bf
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-22 23:59:22 +0200

    Remove unnecessary schema prefixes from SQL query for 'Comedy' shows

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- Affiche tous les shows du genre 'Comedy'. Trie par titre, une seule requête SELECT.
-SELECT tv_shows.title FROM hbtn_0d_tvshows.tv_shows
-JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN hbtn_0d_tvshows.genres ON tv_show_genres.genre_id = genres.id
-WHERE genres.name = 'Comedy'
+SELECT tv_shows.title FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
