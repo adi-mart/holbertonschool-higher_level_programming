8ac1fd3c171a028425962295adb871abc2a4a708
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-22 23:59:16 +0200

    Remove unnecessary schema prefixes from SQL query for 'Dexter' genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- Affiche tous les genres du show 'Dexter'. Trie par nom de genre, une seule requête SELECT.
-SELECT genres.name FROM hbtn_0d_tvshows.tv_shows
-JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN hbtn_0d_tvshows.genres ON tv_show_genres.genre_id = genres.id
+SELECT genres.name FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN genres ON tv_show_genres.genre_id = genres.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY genres.name ASC;
+ORDER BY tv_genres.name ASC;
