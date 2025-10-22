d1810d83e6e5401acd3717d0702d49a67a31083e
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-22 23:30:52 +0200

    Add SQL script to select all cities in California using a subquery

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- Affiche toutes les villes de Californie dans la base 'hbtn_0d_usa'. Utilise une sous-requête, sans JOIN, et trie par id.
+SELECT * FROM cities
+WHERE state_id = (
+    SELECT id FROM states
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
