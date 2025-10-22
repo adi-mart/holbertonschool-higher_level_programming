13c982dca1e7a9e479a3f85061348835bb237ad2
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-22 23:48:02 +0200

    Fix SQL join condition in query to correctly reference 'show_id' for genre associations

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Affiche tous les shows ayant au moins un genre associé. Affiche le titre et l'id du genre, triés par titre et genre.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
