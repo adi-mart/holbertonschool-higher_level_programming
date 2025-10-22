abbf03a331045785cff90c59bf9733158f11322e
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-22 23:34:39 +0200

    Fix user names in SQL script for displaying privileges of 'user_0d_1' and 'user_0d_2'

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Affiche les privilèges des utilisateurs 'users_0d_1' et 'users_0d_2' sur le serveur MySQL.
-SHOW GRANTS FOR 'users_0d_1'@'localhost';
-SHOW GRANTS FOR 'users_0d_2'@'localhost';
+-- Affiche les privilèges des utilisateurs 'user_0d_1' et 'user_0d_2' sur le serveur MySQL.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
