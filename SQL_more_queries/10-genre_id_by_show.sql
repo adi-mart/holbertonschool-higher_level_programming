738d65c33041e96cb49ffeaa58e082f7a2293cfc
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-22 23:43:38 +0200

    Format SQL query for better readability by splitting SELECT statement onto a new line

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- Affiche tous les shows ayant au moins un genre associé. Affiche le titre et l'id du genre, triés par titre et genre.
-SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows.tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM hbtn_0d_tvshows.tv_shows
 JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
