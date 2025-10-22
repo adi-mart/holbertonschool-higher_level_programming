19f6adbb83612589de3ac4f6f1e41523803eab92
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-23 00:05:32 +0200

    Fix SQL query to use correct table name for genres

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Affiche tous les shows et tous les genres associés (NULL si pas de genre). Trie par titre et nom de genre, une seule requête SELECT.
-SELECT tv_shows.title, genres.name 
+SELECT tv_shows.title, tv_genres.name 
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, genres.name ASC;
