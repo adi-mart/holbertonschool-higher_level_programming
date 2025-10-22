b4bdb227a6ac8fdabc499ef515968d1dc6a76d01
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-22 23:31:03 +0200

    Add SQL script to display all shows with associated genres, ordered by title and genre id

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- Affiche tous les shows ayant au moins un genre associé. Affiche le titre et l'id du genre, triés par titre et genre.
+SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows.tv_shows
+JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
