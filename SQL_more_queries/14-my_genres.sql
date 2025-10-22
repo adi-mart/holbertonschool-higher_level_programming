66d20e8225921fd0af997e9d5c2d4219efed52dd
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-23 00:05:21 +0200

    Fix SQL query to use correct table names for 'Dexter' genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- Affiche tous les genres du show 'Dexter'. Trie par nom de genre, une seule requête SELECT.
-SELECT genres.name FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN genres ON tv_show_genres.genre_id = genres.id
+SELECT tv_genres.name FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
