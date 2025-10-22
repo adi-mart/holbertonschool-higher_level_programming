ee0c0eff751e86060112d3c0b7ecd5ab86e2d641
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-23 00:05:16 +0200

    Fix SQL query to use correct table names for genre counting

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Affiche chaque genre et le nombre de shows associés. Trie par nombre de shows décroissant, n'affiche pas les genres sans show.
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_show_genres
-JOIN genres ON tv_show_genres.genre_id = genres.id
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
