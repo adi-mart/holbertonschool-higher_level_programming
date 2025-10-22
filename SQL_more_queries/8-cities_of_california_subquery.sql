45f123e75ccfd5a5fd8fd7a5eb6e01087ffd50cc
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-22 23:41:16 +0200

    Fix SQL query to select specific columns 'id' and 'name' from 'cities' instead of all columns

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Affiche toutes les villes de Californie dans la base 'hbtn_0d_usa'. Utilise une sous-requête, sans JOIN, et trie par id.
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (
     SELECT id FROM states
     WHERE name = 'California'
