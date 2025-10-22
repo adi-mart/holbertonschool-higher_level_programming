655e3d2f70913f0fc612d63a1a897763e42b2015
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-22 23:59:00 +0200

    Format SQL query for better readability by splitting SELECT statement onto a new line

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,5 @@
 -- Affiche tous les shows, même sans genre (NULL si pas de genre). Affiche le titre et l'id du genre, triés par titre et genre.
-SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows.tv_shows
-LEFT JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
