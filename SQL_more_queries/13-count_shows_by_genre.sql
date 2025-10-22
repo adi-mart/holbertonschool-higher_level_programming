de982aa6356f11bd633e2fddf8f4c0c3b045da5d
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-22 23:59:10 +0200

    Fix SQL query to correctly count shows by using 'show_id' and adjust GROUP BY clause

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- Affiche chaque genre et le nombre de shows associés. Trie par nombre de shows décroissant, n'affiche pas les genres sans show.
-SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM hbtn_0d_tvshows.tv_show_genres
-JOIN hbtn_0d_tvshows.genres ON tv_show_genres.genre_id = genres.id
-GROUP BY genres.name
-HAVING number_of_shows > 0
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
+JOIN genres ON tv_show_genres.genre_id = genres.id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
