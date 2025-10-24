5681db660c9102d933fd2ace3c1b46d5ff2ba914
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-24 15:09:11 +0200

    Remove ORDER BY clause from SQL query in filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     # Execute query and fetch results sorted by id (ascending)
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
     rows = cur.fetchall()
 
     for row in rows:
