17c5ff910094adfbc1c703c117d49234bc84cc8a
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-24 16:11:37 +0200

    Add safe filter states script to query database by state name

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument. Safe from MySQL injections!
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=password, db=db_name)
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    cur.execute(query, (state_name,))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
