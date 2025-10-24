6e7cee68c2c3ee3d7364440f353b159b3926f28d
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-24 16:32:49 +0200

    Refactor output formatting in 5-filter_cities.py to print city names in a single line

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -29,8 +29,7 @@ if __name__ == "__main__":
     cur.execute(query, (state_name,))
     rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+    print(", ".join(row[0] for row in rows))
 
     cur.close()
     db.close()
