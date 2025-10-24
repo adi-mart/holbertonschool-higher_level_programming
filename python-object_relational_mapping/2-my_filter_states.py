0a3640c27196409104b6a8d0fdf60d11b8485b80
adi-mart <aurelie.di-martino@holbertonstudents.com>
2025-10-24 15:49:01 +0200

    Fix SQL query execution in filter_states.py to use parameterized query for safety

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,8 +21,9 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     # Execute query and fetch results sorted by id (ascending)
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
-    cur.execute(query, (state_name,))
+    # using .format (unsafe, for demonstration only)
+    cur.execute("SELECT * FROM states WHERE name = '{}'"
+                "ORDER BY id ASC".format(state_name))
     rows = cur.fetchall()
 
     for row in rows:
