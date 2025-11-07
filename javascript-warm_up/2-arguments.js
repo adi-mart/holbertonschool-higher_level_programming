ae97ea80f19d53a0c71d7ae2ac0d1bf90c58e349
Aurélie <aurelie.di-martino@holbertonstudents.com>
2025-11-08 00:16:32 +0100

    Delete javascript-warm_up directory

--- javascript-warm_up/2-arguments.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const { argv } = require('node:process');
-if (argv[3] != null) {
-  console.log('Arguments found');
-} else if (argv[2] != null) {
-  console.log('Argument found');
-} else {
-  console.log('No argument');
-}
